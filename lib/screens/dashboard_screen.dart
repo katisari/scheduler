adc8ad23446a3b1cf80f794771cc7e627165a3e5
Katie Lee <ktjlee@umich.edu>
2020-08-24 17:27:47 -0400

    sign in page

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scheduler/screens/survey_screen.dart';
 import 'package:scheduler/screens/calendar_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 
 class DashboardScreen extends StatefulWidget {
   static const routeName = '/dashboard';
@@ -17,32 +17,56 @@ class _DashboardScreenState extends State<DashboardScreen> {
         appBar: AppBar(
           title: Text('Welcome'),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Column(
-            children: [
-              Text(
-                'Scheduler',
-                style: TextStyle(color: Colors.blue, fontSize: 30.0),
-              ),
-              Text('Turn your tasks into Action'),
-              SizedBox(height: 300.0),
-              RaisedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute<Null>(
-                      builder: (BuildContext context) {
-                        return SurveyScreen();
-                      },
-                    ),
-                  );
-                },
-                child: Text('Next'),
-              )
-            ],
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Skedooler',
+                  style: TextStyle(fontSize: 30.0),
+                ),
+                Text('Turn your Tasks into Action'),
+                SizedBox(height: 50),
+                _signInButton(),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+}
+
+Widget _signInButton() {
+  return OutlineButton(
+    splashColor: Colors.grey,
+    onPressed: () {},
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+    highlightElevation: 0,
+    borderSide: BorderSide(color: Colors.grey),
+    child: Padding(
+      padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image(
+              image: AssetImage('assets/images/google_logo.png'), height: 35.0),
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: Text(
+              'Sign in with Google',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+              ),
+            ),
+          )
+        ],
+      ),
+    ),
+  );
 }
